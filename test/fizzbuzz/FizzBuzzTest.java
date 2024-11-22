16425e8abc91d08bfe9de5462783279c195b1a41
MohamadRammal10 <mohamad.rammal.mr@gmail.com>
2024-11-22 11:24:10 +0100

    Fizzbuzz(5) = buzz

--- test/fizzbuzz/FizzBuzzTest.java
+++ test/fizzbuzz/FizzBuzzTest.java
@@ -20,5 +20,17 @@ class FizzBuzzTest {
 	void fizz_trois() {
 		assertEquals(FizzBuzz.fizzbuzz(3), "fizz");
 	}
+	
+	@Test
+	void fizz_quelques_valeurs() {
+		assertEquals(FizzBuzz.fizzbuzz(4), "4");
+		assertEquals(FizzBuzz.fizzbuzz(7), "7");
+		assertEquals(FizzBuzz.fizzbuzz(400), "400");
+	}
+	
+	@Test
+	void fizz_cinq() {
+		assertEquals(FizzBuzz.fizzbuzz(5), "buzz");
+	}
 
 }
